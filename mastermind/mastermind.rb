f361af59fa4a6e77c76bedbf5b1c2ba1475a6d50
Dave Charnuska <davecharnuska@gmail.com>
2015-06-26 12:35:02 -0400

    begin game class

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -1,4 +1,5 @@
 class Code
+  attr_reader :seq
   COLORS = [:red, :green, :blue, :yellow, :orange, :purple]
   
   def initialize(seq = Code.random)
@@ -16,5 +17,36 @@ class Code
   end
 end
 
-c = Code.new
-p c
+class Game
+  def initialize(seq1, seq2)
+    @seq1, @seq2 = seq1, seq2
+    @turns = 0
+  end
+  
+  def compare(seq1=@seq1, seq2=@seq2)
+    exact, near = 0, 0
+    test1, test2 = seq1.seq.dup, seq2.seq.dup
+    test1.each_index do |idx|
+      if test1[idx] == test2[idx]
+        exact += 1
+        test1[idx] = nil
+        test2[idx] = nil
+      end
+    end
+    p test1
+    p test2
+    test1.select! {|color| !color.nil? }
+    test2.select! {|color| !color.nil?}
+    p test1
+    p test2
+    
+  end
+end
+
+c1 = Code.new
+c2 = Code.new
+p c1
+p c2
+
+g = Game.new(c1, c2)
+g.compare
