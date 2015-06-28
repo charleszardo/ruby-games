a086d51e7327798cac448971136f88fcfd484767
Dave Charnuska <davecharnuska@gmail.com>
2015-06-27 20:52:07 -0400

    finish compare method, begin player classes

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -26,27 +26,67 @@ class Game
   def compare(seq1=@seq1, seq2=@seq2)
     exact, near = 0, 0
     test1, test2 = seq1.seq.dup, seq2.seq.dup
+    test1_hash = {}
+    
     test1.each_index do |idx|
-      if test1[idx] == test2[idx]
+      color1, color2 = test1[idx], test2[idx]
+      if color1 == color2
         exact += 1
         test1[idx] = nil
         test2[idx] = nil
+      else
+        test1_hash[color1] = (test1_hash[color1] || 0) + 1
       end
     end
-    p test1
-    p test2
-    test1.select! {|color| !color.nil? }
-    test2.select! {|color| !color.nil?}
-    p test1
-    p test2
+    
+    test2.each do |color|
+      if test1_hash[color]
+        near += 1
+        test1_hash[color] > 1 ? test1_hash[color] -= 1 : test1_hash.delete(color)
+      end
+    end
+    
+    {:exact => exact, :near => near}
+  end
+  
+  def play
+    correct_code = player1.create_code
     
   end
 end
 
+class Player
+  def create_code
+  end
+end
+
+class Human < Player
+  CODE_CONVERTER = {
+    "R" => :red,
+    "G" => :green,
+    "B" => :blue,
+    "Y" => :yellow,
+    "O" => :orange,
+    "P" => :purple
+  }
+  
+  def create_code
+    puts "give me a code! (format: 'RGYB')"
+    code = gets.chomp
+  end
+  
+  def parse_code
+    
+  end
+end
+
+class Computer < Player
+  
+end
+
+
+
 c1 = Code.new
 c2 = Code.new
-p c1
-p c2
 
 g = Game.new(c1, c2)
-g.compare
