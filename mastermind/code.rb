40d7580f8a3d9f0690f9408277ea658ee7f74350
Dave Charnuska <davecharnuska@gmail.com>
2015-06-29 18:03:04 -0400

    turn mastermind into module

--- mastermind/code.rb
+++ mastermind/code.rb
@@ -4,7 +4,7 @@ class String
   def orange
     self.light_red
   end
-  
+
   def purple
     self.magenta
   end
@@ -13,21 +13,21 @@ end
 class Code
   attr_reader :seq
   COLORS = [:red, :green, :blue, :yellow, :orange, :purple]
-  
+
   def initialize(seq = Code.random)
     @seq = seq
   end
-  
+
   def display
     puts @seq.map { |color| color.to_s.send(color)}.join(" ")
   end
-  
+
   def self.random
     code = []
     4.times { code << COLORS.sample }
     code
   end
-  
+
   def self.seq_check
     #todo
   end
--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -6,7 +6,7 @@ if $PROGRAM_NAME == __FILE__
   p2 = Human.new
   p1 = Computer.new
 
-  g = Game.new(p1, p2)
+  g = Mastermind::Game.new(p1, p2)
   g.play
 end
 
