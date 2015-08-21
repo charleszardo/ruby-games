9267c8328cc1dbe28ec059b850c7114365491c55
Dave Charnuska <davecharnuska@gmail.com>
2015-08-21 12:47:00 -0400

    add computer and human classes

--- memory_puzzle/memory_puzzle.rb
+++ memory_puzzle/memory_puzzle.rb
@@ -68,7 +68,14 @@ class Board
   
   def render
     @grid.each do |row|
-      puts row.map { |card| card.to_s }.join(" | ")
+      row_disp = row.map do |card|
+        card = card.to_s
+        if card.size < 2
+          card += " "
+        end
+        card
+      end
+      puts row_disp.join(" | ")
     end
     puts "\n"
   end
@@ -89,8 +96,8 @@ end
 class Game
   def initialize(player)
     @player = player
-    size = get_difficulty
-    @board = Board.new(size)
+    @size = get_difficulty
+    @board = Board.new(@size)
     @turns = 0
   end
   
@@ -104,17 +111,19 @@ class Game
   end
   
   def get_and_reveal_card
-    puts "select a card"
-    idx = gets.chomp.split(",").map { |num| num.to_i }
-    card = @board[*idx]
+    pos = @player.select_card(@size)
+    card = @board[*pos]
     card.reveal
     card
   end
   
   def turn
+    @board.render
     card1 = get_and_reveal_card
+    @player.receive_match(card1)
     @board.render
     card2 = get_and_reveal_card
+    @player.receive_match(card2)
     @board.render
     if card1 != card2
       card1.hide
@@ -124,12 +133,104 @@ class Game
       puts "MATCH!"
     end
     @turns += 1
+    sleep(5)
     system "clear"
   end
+end
+
+class Player
+  def initialize
+    @prev_guess = nil
+    @current_guess = nil
+    @guesses = {}
+  end
+  
+  def valid_selection?(selection, board_size)
+    pos = selection.split(",")
+    return false if pos.length != 2
+    begin
+      pos.each_with_index do |num, idx|
+        num_int = num.to_i
+        return false if num_int.to_s != num || !num_int.between?(0, board_size - 1)
+        pos[idx] = num_int
+      end
+    rescue NoMethodError
+      return false
+    end
+    pos
+  end
+  
+  def receive_match(card)
     
+  end
+end
+
+class Human < Player
+  def select_card(board_size)
+    puts "select a card (format: 1,2)"
+    selection = nil
+    loop do
+      selection = gets.chomp
+      pos = valid_selection?(selection, board_size)
+      if pos
+        selection = pos
+        break
+      end
+      puts "invalid card. try again."
+    end
+    selection
+  end
+  
+  def select_difficulty
+    loop do
+      puts "select difficulty (4, 6, 8)"
+      begin
+        diff = gets.chomp.to_i
+        return diff if [4, 6, 8].include?(diff)
+      rescue
+      end
+    end
+  end
+end
+
+class Computer < Player
+  
+  def select_card(board_size)
+    selection = nil
+    loop do
+      selection = Array.new(2) { (0...board_size).to_a.sample }
+      if @prev_guess
+        unless @prev_guess == selection
+          @prev_guess = nil
+          break
+        end
+      else
+        @prev_guess = selection
+        break
+      end
+    end
+    
+    @current_guess = selection
+  end
+  
+  def receive_match(card)
+    val = card.value
+    if @guesses[val]
+      @guesses[val][:pos] << @current_guess
+    else
+      @guesses[val] = { :pos => [@current_guess], :matched => false }
+    end
+    p @guesses
+  end
+  
+  def select_difficulty
+    [4, 6, 8].sample
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  g = Game.new
+  h = Human.new
+  c = Computer.new
+  g = Game.new(c)
   g.play
 end
