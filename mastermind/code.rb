d7a2be09b8ce9f2c4bb2f616f396921713facbb9
Dave Charnuska <davecharnuska@gmail.com>
2015-09-02 15:41:29 -0400

    cleanup/refactor

--- mastermind/code.rb
+++ mastermind/code.rb
@@ -19,7 +19,7 @@ class Code
     "O" => :orange,
     "P" => :purple
   }
-  
+
   def self.parse_code(code)
     return nil if !Code.valid_code?(code)
     parsed_code = code.split("").map do |color|
@@ -27,7 +27,7 @@ class Code
     end
     Code.new(parsed_code)
   end
-  
+
   def self.valid_code?(code)
     code = code.split("")
     if code.length != 4
@@ -38,13 +38,13 @@ class Code
       return false
     end
   end
-  
+
   def self.random
     code = []
     4.times { code << CODE_CONVERTER.values.sample }
     Code.new(code)
   end
-  
+
   def initialize(seq)
     @seq = seq
   end
@@ -52,37 +52,36 @@ class Code
   def display
     puts @seq.map { |color| color.to_s.send(color)}.join(" ")
   end
-  
+
   def [](i)
     @seq[i]
   end
-  
+
   def compare(seq2)
     exact, near = 0, 0
     test1, test2 = self.seq.dup, seq2.seq.dup
     test1_hash = {}
-  
+
     test1.each_index do |idx|
       color1, color2 = test1[idx], test2[idx]
       if color1 == color2
         exact += 1
-        test1[idx] = nil
-        test2[idx] = nil
+        test1[idx], test2[idx] = nil, nil
       else
         test1_hash[color1] = (test1_hash[color1] || 0) + 1
       end
     end
-  
+
     test2.each do |color|
       if test1_hash[color]
         near += 1
         test1_hash[color] > 1 ? test1_hash[color] -= 1 : test1_hash.delete(color)
       end
     end
-  
+
     {:exact => exact, :near => near}
   end
-  
+
   protected
   attr_reader :seq
 end
