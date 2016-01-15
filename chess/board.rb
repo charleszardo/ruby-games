e1701c3dab786084fc52b6119bdc86f4c6ca3554
Dave Charnuska <davecharnuska@gmail.com>
2016-01-15 01:26:32 -0500

    begin basic board methods

--- chess/board.rb
+++ chess/board.rb
@@ -2,6 +2,29 @@ class Board
   attr_reader :grid
   
   def initialize
-    @grid = Array.new(8) { Array.new(8) { nil }}
+    size = 8
+    @grid = Array.new(size) do |row|
+      Array.new(size)
+    end
+  end
+  
+  def [](row, col)
+    @grid[row][col]
+  end
+
+  def []=(row, col, val)
+    @grid[row, col] = val
+  end
+  
+  def move(start_pos, end_pos)
+    piece = self[start_pos[0], start_pos[1]]
+    self[start_pos[0], start_pos[1]] = nil
+    self[end_pos[0], end_pos[1]] = piece
+  end
+  
+  def print
+    @grid.each do |row|
+      puts row.join(" | ")
+    end
   end
 end
