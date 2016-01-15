ad1018096e528acf9b651633730cf7ca33b77d8a
Dave Charnuska <davecharnuska@gmail.com>
2016-01-15 16:45:23 -0500

    update piece pos with move method

--- chess/board.rb
+++ chess/board.rb
@@ -6,7 +6,10 @@ class Board
   def initialize
     @size = 8
     @grid = Array.new(size) do |row|
-      Array.new(size) { "x"}
+      Array.new(size) do |col|
+        pos = [row, col]
+        Piece.new(pos)
+      end
     end
   end
   
@@ -21,6 +24,7 @@ class Board
   def move(start_pos, end_pos)
     raise 'invalid move' if !valid_move(start_pos, end_pos)
     piece = self[start_pos[0], start_pos[1]]
+    piece.move(end_pos)
     self[start_pos[0], start_pos[1]] = nil
     self[end_pos[0], end_pos[1]] = piece
   rescue
