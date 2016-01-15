dfc6865799a000fbf070b10ac62c61c660a6a58a
Dave Charnuska <davecharnuska@gmail.com>
2016-01-15 16:41:01 -0500

    add piece class

--- chess/chess.rb
+++ chess/chess.rb
@@ -1,5 +1,15 @@
+require_relative 'piece'
 require_relative 'board'
 
-b = Board.new
-
-p b.grid
+# b = Board.new
+# b.print
+#
+# puts " "
+# b[5,5] = "abc"
+# b.print
+# p b
+# b.move([5,5], [1,1])
+#
+# puts " "
+# b.print
+# p b[5,5]
