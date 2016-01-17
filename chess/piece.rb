b85f117caeed18d4bf20ae3cd3439452704d24ea
Dave Charnuska <davecharnuska@gmail.com>
2016-01-16 23:11:54 -0500

    initialize pieces with colors. reflect color in display. clean up

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,10 +1,11 @@
 class Piece
-  attr_reader :pos, :display
+  def initialize(pos, color)
+    @pos, @color = pos, color
+    @white, @black = "\u0078", "\u0078"
+  end
   
-  def initialize(pos)
-    @sym = "\u0078"
-    @pos = pos
-    @display = @sym.encode('utf-8')
+  def sym
+    @color == :white ? @white : @black
   end
   
   def move(pos)
@@ -14,6 +15,10 @@ class Piece
   def present?
     true
   end
+  
+  def display
+    sym.encode('utf-8')
+  end
 
   def to_s
     " #{display} "
@@ -21,12 +26,9 @@ class Piece
 end
 
 class NullPiece < Piece
-  attr_reader :pos, :display
-  
-  def initialize(pos)
-    @sym = "\u0020"
-    @pos = pos
-    @display = @sym.encode('utf-8')
+  def initialize(pos, color=nil)
+    @pos, @color = pos, color
+    @white, @black = "\u0020", "\u0020"
   end
   
   def present?
@@ -35,85 +37,61 @@ class NullPiece < Piece
 end
 
 class Bishop < Piece
-  attr_reader :pos, :display
-  
-  def initialize(pos)
-    @sym = "\u2657"
-    # black: 265D
-    @pos = pos
-    @display = @sym.encode('utf-8')
+  def initialize(pos, color)
+    @pos, @color = pos, color
+    @white, @black = "\u265D", "\u2657"
   end
 end
 
 class Rook < Piece
-  attr_reader :pos, :display
-  
-  def initialize(pos)
-    @sym = "\u2656"
-    # black: 265C
-    @pos = pos
-    @display = @sym.encode('utf-8')
+  def initialize(pos, color)
+    @pos, @color = pos, color
+    @white, @black = "\u265C", "\u2656"
   end
 end
 
 class Queen < Piece
-  attr_reader :pos, :display
-  
-  def initialize(pos)
-    @sym = "\u2655"
-    # black: 265B
-    @pos = pos
-    @display = @sym.encode('utf-8')
+  def initialize(pos, color)
+    @pos, @color = pos, color
+    @white, @black = "\u265B", "\u2655"
   end
 end
 
 class Knight < Piece
-  attr_reader :pos, :display
-  
-  def initialize(pos)
-    @sym = "\u2658"
-    # black: 265E
-    @pos = pos
-    @display = @sym.encode('utf-8')
+  def initialize(pos, color)
+    @pos, @color = pos, color
+    @white, @black = "\u265E", "\u2658"
   end
 end
 
 class King < Piece
-  attr_reader :pos, :display
-  
-  def initialize(pos)
-    @sym = "\u2654"
-    # black: 265A
-    @pos = pos
-    @display = @sym.encode('utf-8')
+  def initialize(pos, color)
+    @pos, @color = pos, color
+    @white, @black = "\u265A", "\u2654"
   end
 end
 
 class Pawn < Piece
-  attr_reader :pos, :display
-  
-  def initialize(pos)
-    @sym = "\u2659"
-    # black: 265F
-    @pos = pos
-    @display = @sym.encode('utf-8')
+  def initialize(pos, color)
+    @pos, @color = pos, color
+    @white, @black = "\u265F", "\u2659"
   end
 end
 
-# piece = Piece.new([1,1])
-# bishop = Bishop.new([2,2])
-# np = NullPiece.new([3,3])
-# pa = Pawn.new([1,1])
-# ro = Rook.new([1,1])
-# qu = Queen.new([1,1])
-# kn = Knight.new([2,2])
-# ki = King.new([3,3])
-#
-# p piece.to_s
-# p bishop.to_s
-# p np.to_s
-# p pa.to_s
-# p ro.to_s
-# p qu.to_s
-# p kn.to_s
-# p ki.to_s
+piece = Piece.new([1,1], :black)
+bishop = Bishop.new([2,2], :white)
+np = NullPiece.new([3,3])
+pa = Pawn.new([1,1], :black)
+ro = Rook.new([1,1], :white)
+qu = Queen.new([1,1], :black)
+kn = Knight.new([2,2], :white)
+ki = King.new([3,3], :black)
+
+p piece.to_s
+p bishop.to_s
+p np.to_s
+p pa.to_s
+p ro.to_s
+p qu.to_s
+p kn.to_s
+p ki.to_s
