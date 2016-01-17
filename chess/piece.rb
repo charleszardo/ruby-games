986a569d204644952a705cf3a8dd6ec903e1973b
Dave Charnuska <davecharnuska@gmail.com>
2016-01-16 23:20:24 -0500

    add reference to board for all piece classes

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,6 +1,6 @@
 class Piece
-  def initialize(pos, color)
-    @pos, @color = pos, color
+  def initialize(pos, color, board)
+    @pos, @color, @board = pos, color, board
     @white, @black = "\u0078", "\u0078"
   end
   
@@ -26,8 +26,8 @@ class Piece
 end
 
 class NullPiece < Piece
-  def initialize(pos, color=nil)
-    @pos, @color = pos, color
+  def initialize(pos, color=nil, board)
+    @pos, @color, @board = pos, color, board
     @white, @black = "\u0020", "\u0020"
   end
   
@@ -37,69 +37,75 @@ class NullPiece < Piece
 end
 
 class SlidingPiece < Piece
-  def initialize(pos, color=nil)
-    @pos, @color = pos, color
+  def initialize(pos, color=nil, board)
+    @pos, @color, @board = pos, color, board
     @white, @black = "\u0020", "\u0020"
+  end
+  
+  def move_dirs
+    
   end
 end
 
 class SteppingPiece < Piece
-  def initialize(pos, color=nil)
-    @pos, @color = pos, color
+  def initialize(pos, color=nil, board)
+    @pos, @color, @board = pos, color, board
     @white, @black = "\u0020", "\u0020"
   end
 end
 
-class Bishop < Piece
-  def initialize(pos, color)
-    @pos, @color = pos, color
+class Bishop < SlidingPiece
+  def initialize(pos, color, board)
+    @pos, @color, @board = pos, color, board
     @white, @black = "\u265D", "\u2657"
   end
 end
 
-class Rook < Piece
-  def initialize(pos, color)
-    @pos, @color = pos, color
+class Rook < SlidingPiece
+  def initialize(pos, color, board)
+    @pos, @color, @board = pos, color, board
     @white, @black = "\u265C", "\u2656"
   end
 end
 
-class Queen < Piece
-  def initialize(pos, color)
-    @pos, @color = pos, color
+class Queen < SlidingPiece
+  def initialize(pos, color, board)
+    @pos, @color, @board = pos, color, board
     @white, @black = "\u265B", "\u2655"
   end
 end
 
 class Knight < Piece
-  def initialize(pos, color)
-    @pos, @color = pos, color
+  def initialize(pos, color, board)
+    @pos, @color, @board = pos, color, board
     @white, @black = "\u265E", "\u2658"
   end
 end
 
 class King < Piece
-  def initialize(pos, color)
-    @pos, @color = pos, color
+  def initialize(pos, color, board)
+    @pos, @color, @board = pos, color, board
     @white, @black = "\u265A", "\u2654"
   end
 end
 
 class Pawn < Piece
-  def initialize(pos, color)
-    @pos, @color = pos, color
+  def initialize(pos, color, board)
+    @pos, @color, @board = pos, color, board
     @white, @black = "\u265F", "\u2659"
   end
 end
 
-piece = Piece.new([1,1], :black)
-bishop = Bishop.new([2,2], :white)
-np = NullPiece.new([3,3])
-pa = Pawn.new([1,1], :black)
-ro = Rook.new([1,1], :white)
-qu = Queen.new([1,1], :black)
-kn = Knight.new([2,2], :white)
-ki = King.new([3,3], :black)
+b = "board"
+
+piece = Piece.new([1,1], :black, b)
+bishop = Bishop.new([2,2], :white, b)
+np = NullPiece.new([3,3], nil, b)
+pa = Pawn.new([1,1], :black, b)
+ro = Rook.new([1,1], :white, b)
+qu = Queen.new([1,1], :black, b)
+kn = Knight.new([2,2], :white, b)
+ki = King.new([3,3], :black, b)
 
 p piece.to_s
 p bishop.to_s
