ee3ab5516ffd3ee84fc87069aa4b8e93c698b1c1
Dave Charnuska <davecharnuska@gmail.com>
2016-02-04 18:43:41 -0500

    add #add_card and #reveal

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -1,5 +1,17 @@
 class Hand
-  def initialize(cards)
-    @cards = cards
+  def initialize
+    @cards = []
+  end
+  
+  def add_card(card)
+    @cards << card
+  end
+  
+  def reveal
+    revelation = []
+    @cards.each do |card|
+      revelation << [card.suit, card.val]
+    end
+    p revelation
   end
 end
