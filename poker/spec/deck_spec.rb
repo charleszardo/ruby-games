055fa65c5c2667851666b58a1e6352af1c6255bf
Dave Charnuska <davecharnuska@gmail.com>
2016-02-04 18:23:31 -0500

    add spec for shuffling deck

--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -3,9 +3,32 @@ require 'deck'
 
 describe Deck do
   describe "#initialize" do
-    subject(:deck) { Deck.new }
-    it "initializes with 52 cards" do
-      expect(deck.deck.size).to eq(52)
+    subject(:deck1) { Deck.new }
+    subject(:deck2) { Deck.new }
+    subject(:deck3) { Deck.new }
+    subject(:deck4) { Deck.new }
+    subject(:deck5) { Deck.new }
+    
+    it "initializes with 52 unique cards" do
+      deck_hash = {}
+      deck1.deck.each do |card|
+        k = [card.suit, card.val]
+        deck_hash[k] = deck_hash[k] || true
+      end
+      
+      expect(deck1.deck.size).to eq(52)
+      expect(deck_hash.keys.size).to eq(52)
+    end
+    
+    it "shuffles deck" do
+      decks = [deck1, deck2, deck3, deck4, deck5]
+      first_cards = decks.map do |deck|
+        first_card = deck.deck.first
+        first_card = [first_card.suit, first_card.val]
+        first_card
+      end
+      
+      expect(first_cards.uniq.size).to be > 1
     end
   end
 end
