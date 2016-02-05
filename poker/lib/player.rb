278fc47f3674337a83356f5c0d32f7f466e708e9
Dave Charnuska <davecharnuska@gmail.com>
2016-02-05 14:17:53 -0500

    add #receive_coins

--- poker/lib/player.rb
+++ poker/lib/player.rb
@@ -3,10 +3,14 @@ require_relative 'hand'
 class Player
   def initialize
     @hand = Hand.new
+    @coins = 0
   end
   
   def receive_card(card)
     @hand.add_card(card)
-    @hand.reveal
+  end
+  
+  def receive_coins(num)
+    @coins += num
   end
 end
