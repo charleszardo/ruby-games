23cfdae9337649dba4d2d91584b2fc778f4522a0
Dave Charnuska <davecharnuska@gmail.com>
2016-02-05 14:17:33 -0500

    add #setup, #distribute_coins, #round

--- poker/lib/game.rb
+++ poker/lib/game.rb
@@ -6,16 +6,34 @@ class Game
     @player1 = Player.new
     @player2 = Player.new
     @deck = Deck.new
-    @players = [@player1, @player2]
+    @active_players = [@player1, @player2]
+    @init_coins = 20
+    @bet = 1
   end
   
   def play
+    setup
+    round
+  end
+  
+  def setup
+    distribute_coins
     deal_hands
+  end
+  
+  def distribute_coins
+    @active_players.each { |player| player.receive_coins(@init_coins) }
+  end
+  
+  def round
+    @active_players.each do |player|
+      player.peform_action
+    end
   end
   
   def deal_hands
     5.times do
-      @players.each do |player|
+      @active_players.each do |player|
         card = @deck.deal
         player.receive_card(card)
       end
