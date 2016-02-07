b3049e5c2d34b030dcb232fef1f40855597d3b7f
Dave Charnuska <davecharnuska@gmail.com>
2016-02-07 14:22:44 -0500

    add #same_suit? test

--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -2,19 +2,27 @@ require 'rspec'
 require 'hand'
 
 describe Hand do
+  subject(:hand) { Hand.new }
+  let(:card1) { double('card', :suit => :spades) }
+  let(:card2) { double('card', :suit => :spades) }
+  let(:card3) { double('card', :suit => :spades) }
+  let(:card4) { double('card', :suit => :spades) }
+  let(:card5) { double('card', :suit => :hearts) }
+  let(:card6) { double('card', :suit => :diamonds) }
+  
   describe "size" do
-    subject(:hand) { Hand.new }
-    let(:card1) { double('card') }
-    let(:card2) { double('card') }
-    let(:card3) { double('card') }
-    let(:card4) { double('card') }
-    let(:card5) { double('card') }
-    let(:card6) { double('card') }
-    
     it "should never exceed five cards" do
       cards = [card1, card2, card3, card4, card5]
       cards.each { |card| hand.add_card(card) }
       expect { hand.add_card(card6) }.to raise_error
+    end
+  end
+  
+  describe "same_suit?" do
+    it "correctly determines if cards are same suit" do
+      cards = [card1, card2, card3, card4, card5]
+      base_suit = card1.suit
+      expect(cards[0..3].all? { |card| card.suit == base_suit }).to be true
     end
   end
 end
