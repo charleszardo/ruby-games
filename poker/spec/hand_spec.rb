742924eaed96846d1d85554ee794b0b69bd6511c
Dave Charnuska <davecharnuska@gmail.com>
2016-02-08 14:26:21 -0500

    add #four_of_a_kind? spec

--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -117,4 +117,22 @@ describe Hand do
       end
     end
   end
+  
+  describe "#four_of_a_kind?" do
+    let(:card1) { double('card', :suit => :hearts, :val => 2) }
+    let(:card2) { double('card', :suit => :spades, :val => 2) }
+    let(:card3) { double('card', :suit => :clubs, :val => 2) }
+    let(:card4) { double('card', :suit => :diamonds, :val => 2) }
+    let(:card5) { double('card', :suit => :spades, :val => 13) }
+    let(:card6) { double('card', :suit => :hearts, :val => 4) }
+    it "correctly determines a four of a kind" do
+      cards = [card1, card2, card3, card4, card5]
+      cards.each { |card| hand1.add_card(card) }
+      cards = [card1, card2, card3, card5, card6]
+      cards.each { |card| hand2.add_card(card) }
+      
+      expect(hand1.four_of_a_kind?).to be true
+      expect(hand2.four_of_a_kind?).to be false
+    end
+  end
 end
