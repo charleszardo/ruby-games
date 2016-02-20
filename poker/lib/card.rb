b0132d23547822755fc0c7e2593439e1b34ecbab
Dave Charnuska <davecharnuska@gmail.com>
2016-02-20 13:12:11 -0600

    add #suits, #vals, #display

--- poker/lib/card.rb
+++ poker/lib/card.rb
@@ -4,4 +4,32 @@ class Card
   def initialize(suit, val)
     @suit, @val = suit, val
   end
+  
+  def suits
+    { spades: "S",
+      clubs: "C",
+      hearts: "H",
+      diamonds: "D" }
+  end
+  
+  def vals
+    { 1 => "A", 
+      2 => "2",
+      3 => "3",
+      4 => "4",
+      5 => "5",
+      6 => "6",
+      7 => "7",
+      8 => "8",
+      9 => "9",
+     10 => "10",
+     11 => "J",
+     12 => "Q",
+     13 => "K",
+     14 => "A"}
+  end
+  
+  def display
+    vals[val] + suits[suit]
+  end
 end
