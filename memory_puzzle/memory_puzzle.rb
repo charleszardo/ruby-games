fe7c0d57eda9dc75587d24e0e93886f2a55767e5
Dave Charnuska <davecharnuska@gmail.com>
2016-06-15 14:39:24 -0400

    memory working from main menu

--- memory_puzzle/memory_puzzle.rb
+++ memory_puzzle/memory_puzzle.rb
@@ -3,7 +3,7 @@ require_relative 'board'
 require_relative 'player'
 require_relative 'game'
 
-if __FILE__ == $PROGRAM_NAME
+if $PROGRAM_NAME == 'arcade.rb'
   h = Human.new
   c = Computer.new
   g = Game.new(h)
