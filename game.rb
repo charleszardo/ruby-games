8cd2b00e61b5d2bd63698e785f71047859b84034
Dave Charnuska <davecharnuska@gmail.com>
2016-06-22 17:21:51 -0400

    make Game.create_dictionary more flexible

--- game.rb
+++ game.rb
@@ -2,8 +2,14 @@ class Game
   def self.create_dictionary(min_letters=nil, max_letters=nil)
     dictionary = Set.new
     File.open("./dictionary.txt").each_line do |line|
-      l = line.chomp
-      dictionary << l if l.length >= 3
+      valid = true
+      word = line.chomp
+      len = word.length
+      if (min_letters && len < min_letters) || (max_letters && len < max_letters)
+        valid = false
+      end
+      
+      dictionary << word if valid
     end
     dictionary
   end
