b883877f99f16a3c9589ad4fed4e6cbeedd6caad
Dave Charnuska <davecharnuska@gmail.com>
2016-06-22 16:49:52 -0400

    clean up

--- chess/chess.rb
+++ chess/chess.rb
@@ -36,35 +36,4 @@ class Game
   end
 end
 
-if __FILE__ == $PROGRAM_NAME
-  Game.new.run  #
-  # g = Game.new
-  # b = g.board
-  # b.print
-  # p b[[7,3]].moves
-end
-
-# b = Board.new
-# b2 = Board.dup(b)
-# # b.print#
-# # b2.print
-# piece = b2[[1,0]]
-# # p piece.valid_moves
-# b2.move([1,0], [2,0])
-#
-# # b.print
-# b2.print
-# piece = Piece.new([1,1], :black, b)
-# bishop = Bishop.new([2,2], :white, b)
-# np = NullPiece.new([3,3], nil, b)
-# pa = Pawn.new([1,1], :black, b)
-# ro = Rook.new([1,1], :white, b)
-# qu = Queen.new([1,1], :black, b)
-# kn = Knight.new([2,2], :white, b)
-# ki = King.new([3,3], :black, b)
-
-# p b.print
-# d = Display.new(b)
-# d.render
-# p b.in_check?(:white)
-# p bishop.moves
+Game.new.run
