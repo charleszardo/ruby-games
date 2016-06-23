8c6e06c59d6e5dd20b0c1609dc13ba49374391de
Dave Charnuska <davecharnuska@gmail.com>
2016-06-23 14:34:30 -0400

    move Game.create_dictionary logic to dictionary class and return dictionary object

--- game.rb
+++ game.rb
@@ -1,18 +1,8 @@
 require 'set'
+require_relative 'dictionary'
 
 class Game
   def self.create_dictionary(min_letters=nil, max_letters=nil)
-    dictionary = Set.new
-    File.open("./dictionary.txt").each_line do |line|
-      valid = true
-      word = line.chomp
-      len = word.length
-      if (min_letters && len < min_letters) || (max_letters && len < max_letters)
-        valid = false
-      end
-      
-      dictionary << word if valid
-    end
-    dictionary
+    return Dictionary.new(min_letters, max_letters)
   end
 end
