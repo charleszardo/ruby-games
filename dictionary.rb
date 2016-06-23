eefc53a5c2d473e4bfd6e129687d481d66bd8176
Dave Charnuska <davecharnuska@gmail.com>
2016-06-23 14:32:48 -0400

    create dictionary class

---
+++ dictionary.rb
@@ -0,0 +1,24 @@
+require 'set'
+
+class Dictionary
+  def self.create_dictionary(min_letters, max_letters)
+    dictionary = Set.new
+    File.open("./dictionary.txt").each_line do |line|
+      valid = true
+      word = line.chomp
+      len = word.length
+      if (min_letters && len < min_letters) || (max_letters && len < max_letters)
+        valid = false
+      end
+    
+      dictionary << word if valid
+    end
+    dictionary
+  end
+  
+  def initialize(min_letters, max_letters)
+    @min_letters = min_letters
+    @max_letters = max_letters
+    @dict = Dictionary.create_dictionary(min_letters, max_letters)
+  end
+end
